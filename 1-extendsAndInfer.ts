c2ca59677724e81cd87a0631cea14675a546c103
WHIPLASHCZ <2238907215@qq.com>
2024-02-20 23:11:26 +0800

    补全：套路一：模式匹配做提取

--- 1-extendsAndInfer.ts
+++ 1-extendsAndInfer.ts
@@ -52,3 +52,91 @@ type TrimRight<S extends string> = S extends `${infer R}${" " | "\n" | "\t"}`
   : S;
 type Trim<S extends string> = TrimLeft<TrimRight<S>>;
 let trim: Trim<`  a b c  `>;
+
+/**
+ * 函数
+ */
+
+// GetParameters 获取函数的参数类型
+type GetParameters<T extends Function> = T extends (...args: infer P) => unknown
+  ? P
+  : never;
+let gp: GetParameters<(a: string | number) => void>;
+
+// GetReturnType 获取函数的返回值类型
+type GetReturnType<T extends Function> = T extends (
+  ...args: unknown[]
+) => infer R
+  ? R
+  : never;
+let gr: GetReturnType<() => string | number>;
+
+// 获取函数内的this的类型() GetThisParameterType
+class Dong {
+  name: string;
+  constructor() {
+    this.name = "dong";
+  }
+  // 在方法声明时 指定 this 的类型
+  hello(this: Dong) {
+    return "hello, I'm " + this.name;
+  }
+}
+const d = new Dong();
+type GetThisParameterType<T> = T extends (
+  this: infer THIS,
+  ...args: any[]
+) => any
+  ? THIS
+  : never;
+// 若方法声明时指定了 this 的类型 则返回指定时this的类型；
+// 若方法声明时没有指定this的类型，则返回unknown；
+let gtp: GetThisParameterType<typeof d.hello>;
+
+/**
+ * 构造器
+ */
+
+// GetInstanceType 获取类返回到实例对象的类型
+interface Person {
+  name: string;
+}
+//用interface声明类的类型
+interface PersonConstructor {
+  new (name: string): Person;
+}
+//用type声明类的类型
+type PersonConstructor2 = new (name: string) => Person;
+
+class PersonClass implements Person {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const person: Person = new PersonClass("李建国");
+const personClass: PersonConstructor = PersonClass;
+const personClass2: PersonConstructor2 = PersonClass;
+
+// 获取类返回到实例对象的类型
+type GetInstanceType<T extends new (...args: any) => any> = T extends new (
+  ...args: any
+) => infer I
+  ? I
+  : never;
+let gi: GetInstanceType<typeof PersonClass>;
+let gi2: GetInstanceType<PersonConstructor>;
+
+// GetConstructorParameters 提取构造器的参数类型
+type GetConstructorParameters<T extends new (...args: any) => any> =
+  T extends new (...args: infer A) => any ? A : never;
+let gpt: GetConstructorParameters<typeof PersonClass>; //[name:string]
+
+/**
+ * 索引类型
+ */
+
+// GetRefProps 提取 ref 的值的类型
+type GetRefProps<T extends object> = T extends { ref: infer R } ? R : never;
+let grp: GetRefProps<{ ref: "1" }>;
