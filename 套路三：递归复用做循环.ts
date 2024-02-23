0ebfdcbf2dc94daf84beeb853ad079e2d4f7d3b1
hmirror <iqwebrs@gmail.com>
2024-02-23 16:33:16 +0800

    套路三补全

--- 套路三：递归复用做循环.ts
+++ 套路三：递归复用做循环.ts
@@ -1,3 +1,4 @@
+import type { Equal } from "./utils";
 /**
  * Promise 的递归复用
  */
@@ -21,7 +22,7 @@ type ReverseArr<Arr extends any[]> = Arr extends [infer First, ...infer Rest]
 type arr = ReverseArr<[1, 2, 3, 4, 5]>;
 
 // Includes 查找
-type Equal<A, B> = A extends B ? (B extends A ? true : false) : false;
+
 type Includes<Arr extends any[], TargetItem> = Arr extends [
   infer First,
   ...infer Rest
@@ -49,3 +50,58 @@ type BuildArray<
   Ele = unknown,
   Arr extends unknown[] = []
 > = Arr["length"] extends Length ? Arr : BuildArray<Length, Ele, [...Arr, Ele]>;
+let buildarr: BuildArray<10, "a">;
+
+/**
+ * 字符串类型的递归
+ */
+
+// ReplaceAll 替换所有指定字符
+type ReplaceAll<
+  S extends string,
+  Replaced extends string,
+  Replacer extends string
+> = S extends `${infer Prefix}${Replaced}${infer Suffix}`
+  ? ReplaceAll<`${Prefix}${Replacer}${Suffix}`, Replaced, Replacer>
+  : S;
+let replaceall: ReplaceAll<`abbbaaabbbabbabab`, "b", "c">;
+
+// StringToUnion 把字符串字面量类型的每个字符都提取出来组成联合类型
+type StringToUnion<
+  S extends string,
+  Result = never
+> = S extends `${infer First}${infer Rest}`
+  ? StringToUnion<Rest, Result | First>
+  : Result;
+
+type StringToUnion2<S extends string> = S extends `${infer First}${infer Rest}`
+  ? First | StringToUnion2<Rest>
+  : never;
+let stringToUnion: StringToUnion<"abcdc">;
+let stringToUnion2: StringToUnion2<"abcdc">;
+
+// ReverseStr
+type ReverseStr<S extends string> = S extends `${infer First}${infer Rest}`
+  ? `${ReverseStr<Rest>}${First}`
+  : S;
+let reverseStr: ReverseStr<`abcdefg`>;
+
+/**
+ * 对象类型的递归
+ */
+
+// DeepReadonly 所有属性全部只读化
+type DeepReadonly<O extends Record<PropertyKey, any>> = {
+  readonly [Key in keyof O]: O[Key] extends Record<PropertyKey, any>
+    ? DeepReadonly<O[Key]>
+    : O[Key];
+};
+type deepreadonly = DeepReadonly<{
+  a: {
+    b: {
+      c: {};
+    };
+  };
+}>;
+let dro: deepreadonly = { a: { b: { c: {} } } };
+// dro.a.b.c=1; //报错，c是只读
